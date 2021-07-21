505aec870d49f0998d320f8c02a09dce21ea738f
Onionie <gutierrez.onionie@gmail.com>
2021-07-20 19:33:09 -0700

    Day Switch case

--- app.js
+++ app.js
@@ -3,10 +3,45 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  switch (currentDay){
+    case 0:
+      day = "Sunday";
+    break;
+    case 1:
+      day = "Monday";
+    break;
+    case 2:
+      day = "Tuesday";
+    break;
+    case 3:
+      day = "Wednesday";
+    break;
+    case 4:
+      day = "Thursday";
+    break;
+    case 5:
+      day = "Friday";
+    break;
+    case 6:
+      day = "Saturday";
+    break;
+    default:
+    console.log("It's" + currentDay);
+  }
+
+  //EJS render looks for list file in views folder
+  //kindOfDay is a variable in our list.EJS
+  // day is a variable in our node app.js
+  res.render("list", {
+    kindOfDay: day
+  });
 });
 
 app.listen(3000, function(){
