7be5d2aa7aaf46b133b85f4985e6ba7beda74094
Onionie <gutierrez.onionie@gmail.com>
2021-07-21 00:17:38 -0700

    Added CSS

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-var items = ["Beer", "Soda"];
+let items = ["Beer", "Soda"];
 
 //using express
 app.use(bodyParser.urlencoded({extended: true}));
-
+//To use our css, images, sounds, etc.. we use a public folder
+app.use(express.static("public"));
+//To use EJS --npm i EJS
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-  var today  = new Date();
-  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  var day = today.toLocaleDateString("en-US", options);
+  let today  = new Date();
+  let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  let day = today.toLocaleDateString("en-US", options);
 
-  //EJS render looks for list file in views folder
+  //Express render looks for list.ejs file in views folder
   //kindOfDay is a variable in our list.EJS
   // day is a variable in our node app.js
   res.render("list", {
@@ -26,7 +28,7 @@ app.get("/", function(req, res){
   //When we submit data we need a POST method to take action
   app.post("/", function(req, res){
     //parse data that was an input from our html with a name "item"
-    newItem = req.body.item;
+    let newItem = req.body.item;
     //push parsed "newItem" into our "items" array
     items.push(newItem);
     //redirect that info
