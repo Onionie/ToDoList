262ce8ce9ff55b0460aa8da796d7073deb2049d0
Onionie <gutierrez.onionie@gmail.com>
2021-07-20 22:15:20 -0700

    Updating Lists

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
 const app = express();
+
+var items = ["Beer", "Soda"];
+
+//using express
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res){
-  var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-
-  switch (currentDay){
-    case 0:
-      day = "Sunday";
-    break;
-    case 1:
-      day = "Monday";
-    break;
-    case 2:
-      day = "Tuesday";
-    break;
-    case 3:
-      day = "Wednesday";
-    break;
-    case 4:
-      day = "Thursday";
-    break;
-    case 5:
-      day = "Friday";
-    break;
-    case 6:
-      day = "Saturday";
-    break;
-    default:
-    console.log("It's" + currentDay);
-  }
+  var today  = new Date();
+  var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+  var day = today.toLocaleDateString("en-US", options);
 
   //EJS render looks for list file in views folder
   //kindOfDay is a variable in our list.EJS
   // day is a variable in our node app.js
   res.render("list", {
-    kindOfDay: day
+    kindOfDay: day,
+    newListItems: items
+  });
+
+  app.post("/", function(req, res){
+    newItem = req.body.item;
+    items.push(newItem);
+    res.redirect("/");
   });
 });
 
