8784f468329070bdcf0f9a3401cf4bd08d27d7fd
Onionie <gutierrez.onionie@gmail.com>
2021-07-21 17:54:18 -0700

    Work page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Beer", "Soda"];
+let workItems = [];
 
 //using express
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,21 +21,37 @@ app.get("/", function(req, res){
   //kindOfDay is a variable in our list.EJS
   // day is a variable in our node app.js
   res.render("list", {
-    kindOfDay: day,
+    listTitle: day,
     //We're sending our "items" array to our EJS "newListItems"
     newListItems: items
   });
+});
 
-  //When we submit data we need a POST method to take action
-  app.post("/", function(req, res){
-    //parse data that was an input from our html with a name "item"
-    let newItem = req.body.item;
+//When we submit data we need a POST method to take action
+app.post("/", function(req, res){
+  //parse data that was an input from our html with a name "item"
+  let newItem = req.body.item;
+
+  if (req.body.list === "Work"){
+    workItems.push(newItem);
+    res.redirect("/work");
+  }
+  else{
     //push parsed "newItem" into our "items" array
     items.push(newItem);
     //redirect that info
     res.redirect("/");
-  });
+  }
+
 });
+
+app.get("/work", function(req, res){
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems,
+  })
+});
+
 
 app.listen(3000, function(){
     console.log("listening to port 3000");
