6a6b798194b10d03bb10e6fc16f09489237ba918
Onionie <gutierrez.onionie@gmail.com>
2021-08-30 01:39:27 -0700

    Render database items

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static("public"));
 //To use EJS --npm i EJS
 app.set('view engine', 'ejs');
 
-//To connect to mongoose for mongoDB
+//To connect to mongoose for mongoDB creating db name "todoListDB"
 mongoose.connect("mongodb://localhost:27017/todoListDB", {useNewUrlParser: true});
 
 //Make a schema for our items DB
@@ -24,42 +24,54 @@ const itemsSchema = {
   name: String
 };
 
-//Mongoose Model
+//Mongoose Model will create our collection of item, pluralized from "Item"
 //Variable = mongoose.model(<"Singular Collection Name">, <Schema>)
 const Item = mongoose.model("Item", itemsSchema);
 
+//Created Default items 1-3
 const item1 = new Item({
-  name: "This is Defaule item1"
+  name: "This is Default item 1"
 });
 
 const item2 = new Item({
-  name: "This is Defaule item2"
+  name: "This is Default item 2"
 });
 
 const item3 = new Item({
-  name: "This is Defaule item3"
+  name: "This is Default item 3"
 });
 
+//Created an array of items
 const defaultItems = [item1, item2, item3]
 
-Item.insertMany(defaultItems, function(err){
-  if (err){
-    console.log(err);
-  }
-  else{
-    console.log("insert Many complete");
-  }
-});
+
 
 app.get("/", function(req,res){
-  //Express render looks for list.ejs file in views folder
-  //kindOfDay is a variable in our list.EJS
-  // day is a variable in our node app.js
-  res.render("list", {
-    listTitle: "Today",
-    //We're sending our "items" array to our EJS "newListItems"
-    newListItems: items
+  Item.find({}, function(err, foundItems){
+    if(foundItems.length === 0){
+      //Pushing the array consisting of items to our database
+      Item.insertMany(defaultItems, function(err){
+        if (err){
+          console.log(err);
+        }
+        else{
+          console.log("insert Many complete");
+        }
+      });
+      res.redirect("/");
+    }
+    else{
+      //Express render looks for list.ejs file in views folder
+      //kindOfDay is a variable in our list.EJS
+      // day is a variable in our node app.js
+      res.render("list", {
+        listTitle: "Today",
+        //We're sending our founditems to newListItems Front-End
+        newListItems: foundItems
+      });
+    }
   });
+
 });
 
 
@@ -86,7 +98,7 @@ app.get("/work", function(req, res){
   res.render("list", {
     listTitle: "Work List",
     newListItems: workItems,
-  })
+  });
 });
 
 app.get("/about", function(req,res){
